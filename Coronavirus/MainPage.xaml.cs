1855384c909c59bc8626ac35b6903c9e21c6fcec
anilrsln <anilrsln@gmail.com>
2020-10-31 10:43:07 +0300

    First commit

---
+++ Coronavirus/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Coronavirus.Models;
+using Coronavirus.Provider;
+using Coronavirus.Utility;
+using Xamarin.Forms;
+
+namespace Coronavirus
+{
+    public partial class MainPage : ContentPage
+    {
+        ServiceManager<WorldwideStats> manager = new ServiceManager<WorldwideStats>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            try {
+                WorldwideStats result = new WorldwideStats();
+                var tt = Task.Run(async delegate {
+                    result = await manager.Get(Constraints.GetAllUrl, "/");
+                });
+                tt.Wait();
+                if (result != null) {
+                    labelAffectedCountriesNum.Text = result.AffectedCountries.ToString();
+                    labelPopulationNum.Text = result.Population.ToString();
+                    labelCasesNum.Text = result.Cases.ToString();
+                    labelDeathNum.Text = result.Deaths.ToString();
+                    labelRecoveredNum.Text = result.Recovered.ToString();
+                    labelActiveNum.Text = result.Active.ToString();
+                    labelCriticalNum.Text = result.Critical.ToString();
+                }
+            } catch(Exception ex) {
+
+            }
+        }
+    }
+}
